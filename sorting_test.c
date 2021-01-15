192c93e246c41cbc88659d0be5d6d9645fd96c6f
coshcage <58808125+coshcage@users.noreply.github.com>
2021-01-15 18:58:42 +0800

    Add files via upload

---
+++ sorting_test.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "StoneValley-master/src/svstring.h"
+
+#define MYTYPE int
+#define SIZE (1024 * 1024 * 128)
+
+int cbfcmp(const void * px, const void * py)
+{
+	return *(MYTYPE *)px - *(MYTYPE *)py;
+}
+
+int arr[SIZE];
+
+int main(void)
+{
+	size_t i;
+	LARGE_INTEGER la, lb, lc;
+	srand(1);
+	for (i = 0; i < SIZE; ++i)
+		arr[i] = rand();
+	QueryPerformanceFrequency(&lc);
+	QueryPerformanceCounter(&la);
+	qsort(arr, SIZE, sizeof(int), cbfcmp);
+	QueryPerformanceCounter(&lb);
+	printf("%lf\n", (double)(lb.QuadPart - la.QuadPart) / (double)lc.QuadPart);
+
+	srand(1);
+	for (i = 0; i < SIZE; ++i)
+		arr[i] = rand();
+	QueryPerformanceFrequency(&lc);
+	QueryPerformanceCounter(&la);
+	svMergeSort(arr, SIZE, sizeof(int), cbfcmp);
+	QueryPerformanceCounter(&lb);
+	printf("%lf\n", (double)(lb.QuadPart - la.QuadPart) / (double)lc.QuadPart);
+
+	for (i = 0; i < SIZE - 1; ++i)
+		if (arr[i] > arr[i + 1])
+			return 1;
+
+	srand(1);
+	for (i = 0; i < SIZE; ++i)
+		arr[i] = rand();
+	QueryPerformanceFrequency(&lc);
+	QueryPerformanceCounter(&la);
+	svQuickSort(arr, SIZE, sizeof(int), cbfcmp);
+	QueryPerformanceCounter(&lb);
+	printf("%lf\n", (double)(lb.QuadPart - la.QuadPart) / (double)lc.QuadPart);
+
+	for (i = 0; i < SIZE - 1; ++i)
+		if (arr[i] > arr[i + 1])
+			return 1;
+	return 0;
+}
