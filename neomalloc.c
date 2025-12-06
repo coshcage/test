177649817b61c605379941cdfb27c09911642692
John Cage <58808125+coshcage@users.noreply.github.com>
2025-12-06 09:39:25 -0500

    Add files via upload

--- neomalloc.c
+++ neomalloc.c
@@ -28,13 +28,16 @@
  * +=============+--------/
  */
 
+/* Define constant NULL. */
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
 
+/* sizeof(UCHART) == 1. */
 typedef unsigned char * PUCHAR;
 typedef unsigned char   UCHART;
 
+/* Chunk pointer indices. */
 enum en_FreeChunkPointer
 {
 	FCP_PREV = 0,
@@ -42,20 +45,24 @@ enum en_FreeChunkPointer
 	FCP_MAX  = 2
 };
 
+/* Used and unused mark. */
 #define USED false
 #define FREE true
 
+/* Free chunk linked list node. */
 typedef struct st_FreeChunk
 {
 	struct st_FreeChunk * p[FCP_MAX];
 } FREE_CHUNK, * P_FREE_CHUNK;
 
+/* Heap header structure. */
 typedef struct st_HeapHeader
 {
 	size_t size;
 	size_t hshsiz;
 } HEAP_HEADER, * P_HEAP_HEADER;
 
+/* Usable macros. */
 #define MIN_CHUNK_SIZE (sizeof(size_t) * 2 + sizeof(FREE_CHUNK))
 #define HEAP_BEGIN(ph) (sizeof(HEAP_HEADER) + (((P_HEAP_HEADER)(ph))->hshsiz * sizeof(P_HEAP_HEADER)))
 #define ALIGN          (sizeof(size_t) * CHAR_BIT / 4)
@@ -65,6 +72,13 @@ typedef struct st_HeapHeader
 #define FOOT_NOTE(pfc) (*(size_t *)((PUCHAR)(pfc) + (HEAD_NOTE(pfc) & ~(size_t)MASK)))
 #define FREE_MASK      ( (size_t)FREE)
 #define USED_MASK      (~(size_t)USED - 1)
+
+/* File level function declarations. */
+size_t _nmCLZ(size_t n);
+P_FREE_CHUNK * _nmLocateHashTable(P_HEAP_HEADER ph, size_t i);
+void _nmUnlinkChunk(P_HEAP_HEADER ph, P_FREE_CHUNK ptr);
+void * _nmSplitChunk(P_HEAP_HEADER ph, P_FREE_CHUNK pfc, size_t size);
+void _nmPutChunk(P_HEAP_HEADER ph, P_FREE_CHUNK pfc);
 
 size_t _nmCLZ(size_t n)
 {
@@ -91,7 +105,7 @@ P_FREE_CHUNK * _nmLocateHashTable(P_HEAP_HEADER ph, size_t i)
 
 void _nmUnlinkChunk(P_HEAP_HEADER ph, P_FREE_CHUNK ptr)
 {
-	register P_FREE_CHUNK pfc = (P_FREE_CHUNK)ptr;
+	register P_FREE_CHUNK pfc = ptr;
 	register P_FREE_CHUNK * ppfc;
 
 	ppfc = _nmLocateHashTable(ph, _nmCLZ(HEAD_NOTE(pfc)) - _nmCLZ(ph->size));
@@ -111,6 +125,52 @@ void _nmUnlinkChunk(P_HEAP_HEADER ph, P_FREE_CHUNK ptr)
 			pfc = pfc->p[FCP_PREV];
 		}
 		while (pfc != *ppfc);
+	}
+}
+
+void * _nmSplitChunk(P_HEAP_HEADER ph, P_FREE_CHUNK pfc, size_t size)
+{
+	register P_FREE_CHUNK * ppfc;
+	register void * pt;
+	register size_t i;
+	i = HEAD_NOTE(pfc) - size;
+	i &= ~(size_t)MASK;
+	HEAD_NOTE(pfc) = FOOT_NOTE(pfc) = size;
+	pt = pfc;
+	pfc = (P_FREE_CHUNK)((PUCHAR)pfc + size + sizeof(size_t) * 2);
+	HEAD_NOTE(pfc) &= FREE_MASK;
+	FOOT_NOTE(pfc) &= FREE_MASK;
+	
+	/* Search hash table and put new free chunk to the linked list. */
+	ppfc = _nmLocateHashTable(ph, _nmCLZ(i) - _nmCLZ(ph->size));
+	if (NULL != *ppfc)
+	{
+		pfc->p[FCP_NEXT] = *ppfc;
+		pfc->p[FCP_PREV] = (*ppfc)->p[FCP_PREV];
+		(*ppfc)->p[FCP_PREV] = pfc;
+	}
+	*ppfc = pfc;
+	return pt;
+}
+
+void _nmPutChunk(P_HEAP_HEADER ph, P_FREE_CHUNK pfc)
+{
+	if ((_nmCLZ(HEAD_NOTE(pfc) & ~(size_t)MASK) - _nmCLZ(ph->size)) > ph->hshsiz)
+	{
+		HEAD_NOTE(pfc) &= FREE_MASK;
+		FOOT_NOTE(pfc) &= FREE_MASK;
+	}
+	else
+	{
+		register P_FREE_CHUNK * ppfc;
+		ppfc = _nmLocateHashTable(ph, _nmCLZ(HEAD_NOTE(pfc)) - _nmCLZ(ph->size));
+		if (NULL != *ppfc)
+		{
+			pfc->p[FCP_NEXT] = *ppfc;
+			pfc->p[FCP_PREV] = (*ppfc)->p[FCP_PREV];
+			(*ppfc)->p[FCP_PREV] = pfc;
+		}
+		*ppfc = pfc;
 	}
 }
 
@@ -165,13 +225,14 @@ P_HEAP_HEADER nmCreateHeap(void * pbase, size_t size, size_t hshsiz)
 
 P_HEAP_HEADER nmExtendHeap(P_HEAP_HEADER ph, size_t sizincl)
 {
+	
 	return NULL;
 }
 
 void * nmAllocHeap(P_HEAP_HEADER ph, size_t size)
 {
-	size_t j;
-	P_FREE_CHUNK pfc;
+	register size_t j;
+	register P_FREE_CHUNK pfc, pofc;
 	
 	size = ASIZE(size);
 	j = _nmCLZ(size) - _nmCLZ(ph->size);
@@ -184,7 +245,8 @@ void * nmAllocHeap(P_HEAP_HEADER ph, size_t size)
 	pfc = *_nmLocateHashTable(ph, j);
 	
 	/* Search for fit chunk. */
-	while (pfc)
+	pofc = pfc;
+	while (pfc != pofc)
 	{
 		if (size > HEAD_NOTE(pfc))
 			pfc = pfc->p[FCP_NEXT];
@@ -208,31 +270,9 @@ void * nmAllocHeap(P_HEAP_HEADER ph, size_t size)
 			return pfc;
 		}
 		else
-		{	/* Split. */
-			register P_FREE_CHUNK * ppfc;
-			register void * pt;
-			register size_t i;
-			i = HEAD_NOTE(pfc) - size;
-			i &= ~(size_t)MASK;
-			HEAD_NOTE(pfc) = FOOT_NOTE(pfc) = size;
-			pt = pfc;
-			pfc = (P_FREE_CHUNK)((PUCHAR)pfc + size + sizeof(size_t) * 2);
-			HEAD_NOTE(pfc) &= FREE_MASK;
-			FOOT_NOTE(pfc) &= FREE_MASK;
-			
-			/* Put new free chunk to linked list. */
-			j = _nmCLZ(i) - _nmCLZ(ph->size);
-			
-			/* Search hash table. */
-			ppfc = _nmLocateHashTable(ph, j);
-			if (NULL != *ppfc)
-			{
-				pfc->p[FCP_NEXT] = *ppfc;
-				pfc->p[FCP_PREV] = (*ppfc)->p[FCP_PREV];
-				(*ppfc)->p[FCP_PREV] = pfc;
-			}
-			*ppfc = pfc;
-			return pt;
+		{
+			_nmUnlinkChunk(ph, pfc);
+			return _nmSplitChunk(ph, pfc, size); /* Split. */
 		}
 	}
 	
@@ -241,10 +281,10 @@ void * nmAllocHeap(P_HEAP_HEADER ph, size_t size)
 
 void nmFreeHeap(P_HEAP_HEADER ph, void * ptr)
 {
-	P_FREE_CHUNK pfc = (P_FREE_CHUNK)ptr;
-	bool bbtm = FREE, bhed = FREE;
-	size_t upcolsiz, upcolcnt;
-	size_t lwcolsiz, lwcolcnt;
+	register P_FREE_CHUNK pfc = (P_FREE_CHUNK)ptr;
+	register bool bbtm = FREE, bhed = FREE;
+	register size_t upcolsiz, upcolcnt;
+	register size_t lwcolsiz, lwcolcnt;
 	register size_t chksiz;
 	register size_t i;
 	
@@ -301,13 +341,13 @@ void nmFreeHeap(P_HEAP_HEADER ph, void * ptr)
 	
 	chksiz = HEAD_NOTE(pfc) & ~(size_t)MASK;
 	
-	i = upcolsiz + sizeof(size_t) * upcolcnt;
+	i = upcolsiz + sizeof(size_t) * 2 * upcolcnt;
 	
 	pfc = (P_FREE_CHUNK)((PUCHAR)pfc - i + sizeof(size_t));
 	
 	chksiz += i;
 	
-	chksiz += lwcolsiz + sizeof(size_t) * lwcolcnt;
+	chksiz += lwcolsiz + sizeof(size_t) * 2 * lwcolcnt;
 	
 	chksiz -= 2 * sizeof(size_t);
 	
@@ -316,24 +356,74 @@ void nmFreeHeap(P_HEAP_HEADER ph, void * ptr)
 	FOOT_NOTE(pfc) &= FREE_MASK;
 	
 	/* Put free chunk back into hash table or leave it alone. */
-	if ((i = _nmCLZ(HEAD_NOTE(pfc) & ~(size_t)MASK) - _nmCLZ(ph->size)) > ph->hshsiz)
-	{
-		HEAD_NOTE(pfc) &= FREE_MASK;
-		FOOT_NOTE(pfc) &= FREE_MASK;
-	}
-	else
-	{
-		register P_FREE_CHUNK * ppfc;
-		ppfc = _nmLocateHashTable(ph, _nmCLZ(HEAD_NOTE(pfc)) - _nmCLZ(ph->size));
-		if (NULL != *ppfc)
-		{
-			pfc->p[FCP_NEXT] = *ppfc;
-			pfc->p[FCP_PREV] = (*ppfc)->p[FCP_PREV];
-			(*ppfc)->p[FCP_PREV] = pfc;
-		}
-		*ppfc = pfc;
-	}
+	_nmPutChunk(ph, pfc);
 }
 
-void * nmReallocHeap(P_HEAP_HEADER ph, void * ptr, size_t size);
+void * nmReallocHeap(P_HEAP_HEADER ph, void * ptr, size_t size)
+{
+	register bool bbtm;
+	register size_t j;
+	register size_t chksiz;
+	register size_t lwcolsiz, lwcolcnt;
+	
+	size = ASIZE(size);
+	j = _nmCLZ(size) - _nmCLZ(ph->size);
+	
+	/* Definitely cannot allocate. */
+	if (j > _nmCLZ(size))
+		return NULL;
+	
+	if (size < HEAD_NOTE((P_FREE_CHUNK)ptr))
+		return _nmSplitChunk(ph, (P_FREE_CHUNK)ptr, size); /* Split. */
+	else /* Try to collect residues. */
+	{
+		register P_FREE_CHUNK pfc;
+		
+		pfc = (P_FREE_CHUNK)ptr;
+	
+		/* Get lower bound. */
+		chksiz = HEAD_NOTE(pfc) & ~(size_t)MASK;
+		
+		if ((PUCHAR)pfc + chksiz + sizeof(size_t) - (PUCHAR)ph == ph->size)
+			bbtm = USED;
+		
+		lwcolsiz = 0;
+		lwcolcnt = 0;
+		
+		while (FREE == bbtm && (FREE == (*(size_t *)((PUCHAR)pfc + chksiz + sizeof(size_t)) & ~(size_t)MASK)))
+		{
+			pfc = (P_FREE_CHUNK)((PUCHAR)pfc + chksiz + 2 * sizeof(size_t));
+			chksiz = HEAD_NOTE(pfc) & ~(size_t)MASK;
+			lwcolsiz += chksiz;
+			++lwcolcnt;
+			
+			/* Unlink chunk. */
+			_nmUnlinkChunk(ph, pfc);
+		}
+		
+		/* Coalesce downward. */
+		pfc = (P_FREE_CHUNK)ptr;
+		
+		chksiz = HEAD_NOTE(pfc) & ~(size_t)MASK;
+		
+		chksiz += lwcolsiz + sizeof(size_t) * 2 * lwcolcnt;
+		
+		HEAD_NOTE(pfc) = FOOT_NOTE(pfc) = chksiz;
+		
+		if (chksiz >= size)
+		{
+			HEAD_NOTE(pfc) &= USED_MASK;
+			FOOT_NOTE(pfc) &= USED_MASK;
+			return pfc;
+		}
+		else
+		{
+			register void * pr;
+			pr = nmAllocHeap(ph, size);
+			memcpy(pr, pfc, sizeof(HEAD_NOTE(pfc)));
+			_nmPutChunk(ph, pfc);
+			return pr;
+		}
+	}
+}
 
